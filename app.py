ba6f6dfe2c3ddeecb2d7e705c6199f04ce8be82f
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-12-17 21:38:21 +0800

    Call api manager to get the route advice response

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMe
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from dotenv import load_dotenv
 import os
+import services.api_manager as api_manager
 
 # Load environment variables
 load_dotenv()
@@ -44,8 +45,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
+
+        response = api_manager.get_result(event.message.text)
+
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=event.message.text)]))
+        line_bot_api.reply_message_with_http_info( ReplyMessageRequest( reply_token=event.reply_token, messages=[TextMessage(text=response)]))
 
 if __name__ == "__main__":
     app.run()
